cc693f354d2fbbb17271a1d9c3bb1630c559ed5f
lsc <1787319281@qq.com>
2019-10-08 09:56:12 +0800

    new Command parameter input

--- lib/Syinx.cpp
+++ lib/Syinx.cpp
@@ -2,12 +2,18 @@
 #include "SyAdapter.h"
 #include "SyPthreadAdapter.h"
 #include "SyTaskAdapter.h"
+#include "SyPthreadPool.h"
 #include "SyResAdapter.h"
+
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/un.h>
 
 #ifdef SYINXMOD_ADD_MYSQL
 #include <mysql/mysql.h>
 #else
 #endif 
+
 
 using namespace std;
 
@@ -16,7 +22,7 @@ SyinxKernel* SyinxKernel::mSyinx = nullptr;
 //设置默认为零
 int SyinxKernel::SyDatabaseMod = 0;
 
-SyinxRecycle recycle;
+
 
 
 //listen回调函数传入的参数
@@ -57,13 +63,15 @@ void SyinxKernel_Listen_CB(struct evconnlistener* listener, evutil_socket_t fd, 
 	//框架地址
 	SyinxKernel* mSyinx = poSyinxDeliver->iSyinx;
 
-	//test
-	std::cout << "new client is connect  " << "Client:" << fd << std::endl;
+	if (mSyinx->mShmData->ExitSignal == true)
+	{
+		SyinxKernel::SyinxKernel_Close();
+	}
 
 	//将将新来的客户端委托资源管理器来管理
 	mSyinx->mSyResource->SyinxAdapterResource_AllotClient(fd);
-	
-	
+
+
 
 }
 int SyinxKernel::SyinxKernel_Init(const short _inPort)
@@ -181,63 +189,216 @@ void SyinxKernel::SyinxKernel_Run()
 
 void SyinxKernel::SyinxKernel_Close()
 {
-	//关闭
+	//关闭监听
 	evconnlistener_free(mSyinx->SyinxListen);
 	event_base_free(mSyinx->SyinxBase);
+
+	//free
+	if (SyinxKernel::mSyinx->mSyPth != NULL)
+	{
+		SyinxKernel::mSyinx->mSyPth->SyinxAdapter_Pth_destroy();
+		delete  SyinxKernel::mSyinx->mSyPth;
+	}
+
+	if (SyinxKernel::mSyinx->mSyResource != NULL)
+	{
+		SyinxKernel::mSyinx->mSyResource->SyinxAdapterResource_Free();
+		delete SyinxKernel::mSyinx->mSyResource;
+	}
+
+
+	if (SyinxKernel::mSyinx != NULL)
+		delete SyinxKernel::mSyinx;
+
+	
+
+	SyinxKernel::SyinxSyinxKernel_FreeShm();
+
+	cout << "Syinx is close" << endl;
 }
 
 void SyinxKernel::SyinxKernel_Client_Close(const int mClient_fd)
 {
 }
 
-
-SyinxRecycle::SyinxRecycle()
+void SyinxKernel::SyinxSyinxKernel_MakeShm()
 {
-}
-SyinxRecycle::~SyinxRecycle()
-{
-	mLog.Log(__FILE__, __LINE__, SyinxLog::ERROR, 0, "SyinxRecycle delete all Res!");
-	std::cout << "析构函数被触发" << std::endl;
-	delete SyinxKernel::mSyinx;
-	delete  SyinxKernel::mSyinx->mSyPth;
-	delete SyinxKernel::mSyinx->mSyMission;
-	delete SyinxKernel::mSyinx->mSyResource;
-}
-
-void Printfinterface()
-{
-
-
-	printf("---------------------------------------------------------------\n");
-	printf("                Welcome To Use Syinx \n");
-	printf("---------------------------------------------------------------\n");
-	if (true)
+	int shmid = shmget(SET_SHM_KEY, sizeof(SyinxKernelShmMsg), IPC_CREAT | IPC_EXCL | 0664);
+	if (-1 == shmid)
 	{
-		//绿色状态
-		cout << "Syinx-status:" << "\033[32;1m  run  \033[0m";
+		perror("shmget is failed");
+		exit(0);
+	}
+	SyinxKernel::mSyinx->ShmId = shmid;
+	void* ShmData = shmat(shmid, NULL, 0);
+	mSyinx->ShmData = ShmData;
+	memset(ShmData, 0, sizeof(SyinxKernelShmMsg));
+	auto mShmData = (SyinxKernelShmMsg*)ShmData;
+
+	for (int i = 0; i < mSyinx->PthNum; ++i)
+	{
+		mShmData->threads[i] = mSyinx->mSyPth->mSyinxPriPthPool->threads[i];
+	}
+
+	mShmData->PthNum = mSyinx->PthNum;
+	mShmData->SyinxKernewWork = true;
+	mShmData->ExitSignal = false;
+	strcpy(mShmData->IP, mSyinx->Server_Sockaddr.sin_addr.c_str());
+	strcpy(mShmData->Port, mSyinx->Server_Sockaddr.Prot.c_str());
+
+	mSyinx->mShmData = mShmData;
+}
+
+
+void SyinxKernel::SyinxSyinxKernel_FreeShm()
+{
+	if (shmctl(mSyinx->ShmId, IPC_RMID, NULL) == -1)
+	{
+		perror("shmctl delete is failed");
+		return;
+	}
+}
+
+void SyinxKernel::SyinxSyinxKernel_LocalSock()
+{
+
+
+}
+
+
+
+
+void SyinxKernelWork::PrintfServerStatus()
+{
+
+	int shmid = shmget(SET_SHM_KEY, 0, 0);
+	if (-1 == shmid)
+	{
+		perror("Syinx is not run");
+		exit(0);
+	}
+	void* ShmData = shmat(shmid, NULL, SHM_RDONLY);
+	auto mShmData = (SyinxKernelShmMsg*)ShmData;
+	if (mShmData->SyinxKernewWork)
+	{
+		printf("\n");
+		cout << "Syinx-status:" << "\033[32;1m  run  \033[0m" << endl;
 	}
 	else
 	{
-		//红色状态
-		cout << "Syinx-status:" << "\033[31;1m  close  \033[0m";
+		
+		cout << "Syinx-status:" << "\033[31;1m  close  \033[0m" << endl;
+		exit(0);
 	}
-	printf("Version:0.1.1 IP:[%s] Prot:[%s]\n");
-
-	printf("WorkPid        state        Send      Recv        active\n");
-	for (int i = 0; i < 4; ++i)
+	printf("IP:[%s] Prot:[%s]\n",mShmData->IP, mShmData->Port);
+	printf("Total server connections : [%d]\n", mShmData->AllClientNum);
+	printf("--------------------------------------------------------------------------\n");
+	printf("WorkTid           Status            Connects           Capacity\n");
+	for (int i = 0; i < mShmData->PthNum; ++i)
 	{
+		printf("%X           run                %d                   \n", mShmData->threads[i], mShmData->CurrentClientNum[i]);
+	}
+	
+	printf("\n");
+	if (shmdt(ShmData) == -1)
+	{
+		perror("shmdt is failed");
+		return;
+	}
+}
+void SyinxKernelWork::Makedaemon(int Prot)
+{
+	int pid_t;
+	pid_t = fork();
+	if (-1 == pid_t)
+	{
+		perror("ERR : fork()");
+		exit(0);
+	}
 
+	else if (pid_t > 0)
+		exit(0);
+
+	setsid();
+	chdir("./");
+	umask(0);
+
+	SyinxKernel::SyinxKernel_Init(Prot);
+	this->mSyinx = SyinxKernel::mSyinx;
+	this->SyinxKernelStatus = true;
+	SyinxKernel::SyinxSyinxKernel_MakeShm();
+	SyinxKernel::SyinxKernel_Run();
+
+
+}
+void SyinxKernelWork::SyinxExit()
+{
+	int shmid = shmget(SET_SHM_KEY, 0, 0);
+	if (-1 == shmid)
+	{
+		perror("Syinx is not run");
+		exit(0);
+	}
+	void* ShmData = shmat(shmid, NULL, 0);
+	auto mShmData = (SyinxKernelShmMsg*)ShmData;
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(Port);
+	inet_pton(AF_INET, "192.168.12.31", &addr.sin_addr.s_addr);
+
+	int _fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	int iRet = connect(_fd, (const sockaddr*)& addr, sizeof(addr));
+	mShmData->ExitSignal = true;
+	close(_fd);
+
+	if (shmdt(ShmData) == -1)
+	{
+		perror("shmdt is failed");
+		return;
+	}
+
+
+}
+SyinxKernelWork::SyinxKernelWork(int Prot, int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "ERR : Syinx Uncarried parameter" << endl;
+		exit(0);
+	}
+	this->Port = Prot;
+	if (!strcmp(COMMMEND_PARAM, "run"))
+	{
+		this->Makedaemon(Prot);
+	}
+	else if (!strcmp(COMMMEND_PARAM, "-s"))
+	{
+		//打印状态
+		this->PrintfServerStatus();
+		exit(0);
+	}
+	else if (!strcmp(COMMMEND_PARAM, "-v") | !strcmp(COMMMEND_PARAM, "version"))
+	{
+		cout << "Syinx Version:0.1.4" << endl;
+		exit(0);
+	}
+	else if (!strcmp(COMMMEND_PARAM, "-c") | !strcmp(COMMMEND_PARAM, "close"))
+	{
+		this->SyinxExit();
+		exit(0);
+	}
+	else
+	{
+		cout << "ERR : Syinx Error carrying parameter" << endl;
+		exit(0);
 	}
 
 }
-SyinxKernelWork::SyinxKernelWork(int Prot)
+
+SyinxKernelWork::~SyinxKernelWork()
 {
-	SyinxKernel::SyinxKernel_Init(Prot);
-	Printfinterface();
-	SyinxKernel::SyinxKernel_Run();
+	
 }
 
-int SyinxKernelWork::SyinxClose()
-{
-	SyinxKernel::SyinxKernel_Close();
-}
--- lib/Syinx.h
+++ lib/Syinx.h
@@ -12,21 +12,43 @@
 #define SET_SOCKETS_EVENT_RDWR                          (EV_READ | EV_WRITE | EV_PERSIST)
 //设置读循环
 #define SET_SOCKETS_EVENT_RD                            (EV_READ | EV_PERSIST)
+//命令参数
+#define COMMMEND_PARAM                                   argv[1]
+#define SET_SHM_KEY                                      0x8855
 
 #define SYINXMOD_ADD_MYSQL                               
-#define SYINXMOD_ADD_ORACLE                              
+#define SYINXMOD_ADD_ORACLE    
+
+//框架全部信息用于进程通信
+struct SyinxKernelShmMsg
+{
+	bool SyinxKernewWork;     /*保存当前服务器状态*/
+
+	int PthNum;               /*保存当前开启的主任务数量*/
+
+	int AllClientNum;         /*所有客户端连接数量*/
+
+	int CurrentClientNum[4];
+	pthread_t threads[4];       /*保存每一个tid*/
+
+	char IP[16];                /*保存ip*/
+
+	char Port[8];               /*保存端口号*/
+
+	bool ExitSignal;              /*syinx退出信号*/
+
+}; 
+
 class SyinxLog;
 class IChannel;
 class SyinxAdapterMission;
 class SyinxAdapterPth;
 class SyinxAdapterResource;
+class SyinxPthreadPool;
 
-struct SyinxKernelMsg
-{
-	int mClient_fd;
-	struct buffereveant* Client_buffer;
-};
 class SyinxKernel;
+struct SyinxKernelShmMsg;
+
 class SyinxKernelObject
 {
 public:
@@ -38,9 +60,11 @@ private:
 //用于初始化的核心框架
 class SyinxKernel 
 {
+	friend class SyinxKernelWork;
 	friend class SyinxAdapterPth;
 	friend class SyinxRecycle;
 	friend class SyinxAdapterResource;
+	friend void SyinxKernel_Listen_CB(struct evconnlistener* listener, evutil_socket_t fd, struct sockaddr* sock, int socklen, void* arg);
 public:
 	
 	//初始化框架
@@ -55,6 +79,16 @@ public:
 
 	//客户端退出时将会释放客户端任务链,客户端文件描述符为key值       已废弃
 	static void SyinxKernel_Client_Close(const int mClient_fd);
+
+	//通过共享内存共享数据情报
+	//创建共享内存
+	static void SyinxSyinxKernel_MakeShm();
+	
+	//释放共享内存
+	static void SyinxSyinxKernel_FreeShm();
+
+	//创建初始化本地套接字
+	static void SyinxSyinxKernel_LocalSock();
 
 	SyinxLog mLog;
 private:
@@ -73,6 +107,16 @@ private:
 
 	//客户端port
 	short _Port;
+
+	//保存一个共享内存的shmid
+	int ShmId;
+
+	//保存共享内存的万能指针
+	void* ShmData;
+	SyinxKernelShmMsg* mShmData;
+	
+	//保存本套接字
+	int LocalSocket;
 public:
 	//绑定一份一个SyinxAdapterPt线程适配器
 	SyinxAdapterPth* mSyPth;
@@ -80,7 +124,7 @@ public:
 	SyinxAdapterMission* mSyMission;
 	//绑定一个资源适配器
 	SyinxAdapterResource* mSyResource;
-	
+
 public:
 	int PthNum = 1;
 	//保存服务器的sockaddr_in
@@ -108,28 +152,34 @@ private:
 	int SyinxAddDatabase();
 
 };
-//托管核心初始化开启类
+//托管核心初始化开启类,以及判断输入参数
 class SyinxKernelWork
 {
 public:
 	SyinxKernelWork(){}
-	SyinxKernelWork(int Prot = 8080);
-	~SyinxKernelWork(){}
-
-	int SyinxClose();
-};
-//回收
-class SyinxRecycle
-{
+	SyinxKernelWork(int Prot, int argc, char* argv[]);
+	~SyinxKernelWork();
 public:
-	SyinxRecycle();
-	~SyinxRecycle();
-	SyinxLog mLog;
-	/*
-	1.static std::multimap<int, SyinxKernelMsg*> mZinxMap; 中的SyinxKernelMsg需要释放
-	2.
-	*/
+	
+	//打印服务器状态  -s
+	void PrintfServerStatus();
 
+	
+	void Makedaemon(int Prot);
+
+	void SyinxExit();
+
+	//保存框架的地址
+	SyinxKernel* mSyinx;
+
+	int Port;
+
+	//保存进程id
+	int Pid_t;
+
+	bool SyinxKernelStatus = false;
+	
+	int SyinxKernelExit;
 };
 
 #endif 
