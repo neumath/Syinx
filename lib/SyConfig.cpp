399e4a9770ecb0791680fb80057188969ad25fab
lsc <1787319281@qq.com>
2019-10-18 21:41:11 +0800

    modify bug

--- lib/SyConfig.cpp
+++ lib/SyConfig.cpp
@@ -1,8 +1,7 @@
-
 #include <fstream>
 #include <istream>
 #include <iostream>
-#include<time.h>
+#include <time.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
 #include <fcntl.h>
@@ -11,13 +10,9 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <unistd.h>
-#include <jsoncpp/json/json.h>
 #include "SyLog.h"
 #include "SyConfig.h"
 using namespace std;
-
-
-
 
 SyinxConfig::SyinxConfig()
 {
@@ -25,84 +20,56 @@ SyinxConfig::SyinxConfig()
 SyinxConfig::~SyinxConfig()
 {
 }
-void SyinxConfig::Make_Msgconfig()
+SyinxConfMsg* SyinxConfig::Read_Msgconfig()
 {
-	auto Dir = opendir("./conf");
-	if (Dir == NULL)
+	fstream _fconf;
+	_fconf.open("./conf/Syinx-server.conf", ios::in);
+	if (!_fconf.is_open())
 	{
-		int DirVal = S_IRUSR | S_IWUSR | S_IXUSR ;
-		mkdir("./conf", DirVal);
+		cout << "conf is not find" << endl;
+		return NULL;
+	}
+	mkdir("log", S_IRWXU | S_IRGRP | S_IROTH);
+	string line;
+	SyinxConfMsg* sMsg = new SyinxConfMsg;
+
+
+	while (!_fconf.eof())
+	{
+		getline(_fconf, line);
+		if (line[0] == '#')
+			continue;
+		if (line[0] == ' ')
+			continue;
+		if (line.size() == 0)
+			continue;
+		string _Key = line.substr(0, line.find(':'));
+		string _Value = line.substr(line.find(':') + 1, line.size() - line.find(':'));
+		
+
+		if (_Key == "Host")
+			sMsg->Host = _Value;
+		if (_Key == "Port")
+			sMsg->Port = _Value;
+		if (_Key == "SetPthNum")
+			sMsg->PthNum = stoi(_Value);
+		if (_Key == "SetTaskMax")
+			sMsg->TaskNum = stoi(_Value);
+		if (_Key == "SetTimeinterval")
+			sMsg->Timerinterval = stoi(_Value);
+		if (_Key == "SetTimevalue")
+			sMsg->Timervalue = stoi(_Value);
 	}
 
-	do
-	{
-		int iRet = access("./conf/Syinx.json", F_OK);
-		if (iRet <= 0)
-		{
-			mLog.Log(__FILE__, __LINE__, SyinxLog::ERROR, -1, "Syinx.json not find");
-			
-			break;
-		}
-		else
-		{
-			//读取conf
-			this->Reader_Msgconfig();
-			cout << "json find" << endl;
-			return;
-		}
-	} while (0);
-	//没有就初始化一个配置文件
-	int File_Fd = open("./conf/Syinx.json", O_WRONLY | O_CREAT);
-	if (File_Fd <= 0)
-	{
-		//log
-		return;
-	}
-	close(File_Fd);
-	/*
-	{
-
-	设置用户:char
-	服务器端口:int//默认绑定8800端口
-
-	服务器集群数量:int
-	#数量必须与Group的server的数量一致
-	Group:(char)服务器集群名字{
-			server#1:(int)id
-			server#2:
-	}
-}*/
-	Json::Value root;
-	root["User"] = Json::Value("王大锤");
-	root["Port"] = Json::Value(8080);
-	root["Cluster number"] = Json::Value(1);
-	
-	
-	root["Group Name"] = Json::Value("admin");
-	//集群子节点
-	Json::Value Group;
-	Group["server#1"] = Json::Value(1001);
-	root["Group"] = Json::Value(Group);
-
-
-	cout << "FastWriter:" << endl;
-	Json::FastWriter fw;
-	cout << fw.write(root) << endl << endl;
-
-	//写入
-	Json::StyledWriter sw;
-	ofstream WriteJson;
-	WriteJson.open("./conf/Syinx.json");
-	WriteJson << sw.write(root);
-	WriteJson.close();
-	closedir(Dir);
-	
-
-}
-void SyinxConfig::Reader_Msgconfig()
-{
+#ifdef CONFTEST
+	cout << sMsg->Host << endl;
+	cout << sMsg->Port << endl;
+	cout << sMsg->TreeNum << endl;
+	cout << sMsg->TreeCap << endl;
+	cout << sMsg->loadbalancing << endl;
+	cout << sMsg->Timerinterval << endl;
+	cout << sMsg->Timervalue << endl;
+#endif
+	return sMsg;
 }
 
-void SyinxConfig::Writer_MSgconfig()
-{
-}
--- lib/SyConfig.h
+++ lib/SyConfig.h
@@ -1,19 +1,27 @@
 #ifndef _SYCONFIG_H_
 #define _SYCONFIG_H_ 
 class SyinxLog;
+struct SyinxConfMsg {
+
+	std::string Host;                //ip
+	std::string Port;                //端口
+	int    PthNum;             //设置epoll树数量
+	int    TaskNum;             //设置epoll树容量
+
+
+	int    Timerinterval;       //设置计时器周期时间
+	int    Timervalue;          //计时器第一次延迟事件
+
+};
 class SyinxConfig {
 public:
 	SyinxConfig();
 	~SyinxConfig();
 
 public:
-	//配置json文件
-	void Make_Msgconfig();
-	//读取json文件
-	void Reader_Msgconfig();
-private:
-	//写入json文件
-	void Writer_MSgconfig();
+	//读取配置文件
+	SyinxConfMsg* Read_Msgconfig();
+
 public:
 	SyinxLog mLog;
 };
